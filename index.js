5b5e8cb81f7d75a2b8d23fe1e50fd61c45963324
Vinayak Dagade <vinayak@gmail.com>
2024-12-23 01:05:49 +0530

    url routed added in index.js with auth middleware

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express= require('express');
 const app = express();
-const urlRouter = require('./routes/url');
 const { connectMongoDb } = require('./connection');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const {restirctToLoggedInUserOnly, checkAuth} = require('./middleware/auth');
+
+// routes
+const urlRouter = require('./routes/url');
 const staticRouter = require('./routes/viewRouter');
+const userRoute = require("./routes/user");
 
 //Connection
 connectMongoDb("mongodb://127.0.0.1:27017/url-shortner").then(() => console.log('MongoDb connected'));
@@ -14,10 +19,12 @@ app.set('views', path.resolve('./views'));
 
 //middleware
 app.use(express.urlencoded({extended: false }));
+app.use(cookieParser());
 app.use(express.json());
 
 //routes
-app.use('/url', urlRouter);
-app.use('/', staticRouter);
+app.use('/url', restirctToLoggedInUserOnly, urlRouter);
+app.use('/user', userRoute);
+app.use('/', checkAuth, staticRouter);
 
 app.listen(8001, ()=> console.log('server started'));
